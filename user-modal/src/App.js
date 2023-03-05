d42fbb730989b11f42b68d07bf919bb532bbb32c
Ankit Sharma <sharmaa7861@gmail.com>
2023-03-06 00:16:59 +0530

    adding goals_bucket

---
+++ user-modal/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import AddUser from './Components/Users/AddUser';
+function App() {
+  return (
+    <div>
+      <h2>Hello All Users</h2>
+      <AddUser />
+    </div>
+  );
+}
+
+export default App;
