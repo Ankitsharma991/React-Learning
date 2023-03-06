884163335d4412d1e329ab76d2e7ee38084b7535
Ankit Sharma <sharmaa7861@gmail.com>
2023-03-06 12:19:55 +0530

    finishing user_modal

--- user-modal/src/App.js
+++ user-modal/src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import AddUser from './Components/Users/AddUser';
+import AddUser from "./Components/Users/AddUser";
+import UserList from "./Components/Users/UserList";
+
 function App() {
+  const [usersList, setUserList] = useState([]);
+
+  const addUserHandler = (uName, uAge) => {
+    setUserList((prevUserList) => {
+      return [...prevUserList, { name: uName, age: uAge, id : Math.random().toString() }];
+    });
+  }; 
+
   return (
     <div>
-      <h2>Hello All Users</h2>
-      <AddUser />
+      <AddUser onAddUser={addUserHandler} />
+      <UserList users={usersList} />
     </div>
   );
 }
--- user-modal/src/Components/UI/Button.js
+++ user-modal/src/Components/UI/Button.js
@@ -1,5 +1,5 @@
 import React from "react";
-import classes from "./Card.module.css";
+import classes from "./Button.module.css";
 
 const Button = (props) => {
   return (
