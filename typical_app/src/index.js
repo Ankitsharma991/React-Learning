9fa2dc128018f44a6252a0d6719a107cb5109636
Ankit Sharma <sharmaa7861@gmail.com>
2023-03-12 02:16:59 +0530

    typical page learning completed

--- typical_app/src/index.js
+++ typical_app/src/index.js
@@ -1,8 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import './index.css';
-import App from './App';
+import "./index.css";
+import App from "./App";
+import {AuthContextProvider} from "./store/auth-context";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(<AuthContextProvider><App /></AuthContextProvider>);
+
