d551d716c848f2057cec5eda6657831cd919f107
Ankit Sharma <sharmaa7861@gmail.com>
2023-03-26 23:07:42 +0530

    completed user input and firebase integration

--- eat_on_the_web/src/components/Layout/Header.js
+++ eat_on_the_web/src/components/Layout/Header.js
@@ -1,26 +1,28 @@
-import { Fragment } from "react";
+import { Fragment } from 'react';
 
-import HeaderCartButton from "./HeaderCartButton";
-import mealsImage from "../../assets/meals.jpg";
-import classes from "./Header.module.css";
+import HeaderCartButton from './HeaderCartButton';
+import mealsImage from '../../assets/meals.jpg';
+import classes from './Header.module.css';
 
 const Header = (props) => {
   return (
     <Fragment>
       <header className={classes.header}>
         <h1>
-          <span style={{color:"white"}}>Eat</span>
-          <span style={{color:"yellow"}}>On</span>
-          <span style={{color:"green"}}>The</span>
-          <span style={{color:"blue"}}>Web</span>
+          <span style={{color:'red'}}>Eat</span>
+          <span style={{color:'orange'}}>On</span>
+          <span style={{color:'green'}}>The</span>
+          <span style={{color:'wheat'}}>Web</span>
         </h1>
         <HeaderCartButton onClick={props.onShowCart} />
       </header>
-      <div className={classes["main-image"]}>
-        <img src={mealsImage} alt="A table full of delicious food!" />
+      <div className={classes['main-image']}>
+        <img src={mealsImage} alt='A table full of delicious food!' />
       </div>
     </Fragment>
   );
 };
 
 export default Header;
+
+// https://tasks-4f39d-default-rtdb.firebaseio.com/meals.json
