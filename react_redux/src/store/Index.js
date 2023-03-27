afafb3ff16471bdf5dbffef2fe1e1018db3719cf
Ankit Sharma <sharmaa7861@gmail.com>
2023-03-28 00:36:03 +0530

    updating changes

---
+++ react_redux/src/store/Index.js
@@ -0,0 +1,50 @@
+import { createStore } from "redux";
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = { counter: 0, showCounter: true };
+
+createSlice({
+  name: "counter",
+  initialState: initialState,
+  reducers: {
+    increment() {},
+    decrement() {},
+    increase() {},
+    toggleCounter() {},
+  },
+});
+
+const counterReducer = (state = initialState, action) => {
+  if (action.type === "increment") {
+    return {
+      counter: state.counter + 1,
+      showCounter: state.showCounter,
+    };
+  }
+
+  if (action.type === "increase") {
+    return {
+      counter: state.counter + action.amount,
+      showCounter: state.showCounter,
+    };
+  }
+
+  if (action.type === "decrement") {
+    return {
+      counter: state.counter - 1,
+      showCounter: state.showCounter,
+    };
+  }
+  if (action.type === "toggle") {
+    return {
+      showCounter: !state.showCounter,
+      counter: state.counter,
+    };
+  }
+
+  return state;
+};
+
+const store = createStore(counterReducer);
+
+export default store;
