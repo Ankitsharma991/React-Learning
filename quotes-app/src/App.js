5355ed128e78f24eb8214fa5f94464e806bf8806
Ankit Sharma <sharmaa7861@gmail.com>
2023-04-02 09:44:10 +0530

    initial commit

---
+++ quotes-app/src/App.js
@@ -0,0 +1,13 @@
+import { Route, Switch } from "react-router-dom";
+
+function App() {
+  return (
+    <Switch>
+      <Route path="/quotes"></Route>
+      <Route path="/quotes/:quoteId"></Route>
+      <Route path="/new-quote"></Route>
+    </Switch>
+  );
+}
+
+export default App;
