d2b0ac3835b49844d8e73e89811736df24b42c97
Ankit Sharma <sharmaa7861@gmail.com>
2023-04-15 22:59:30 +0530

    login feature added

--- react-auth/src/App.js
+++ react-auth/src/App.js
@@ -1,10 +1,25 @@
-import "./App.css";
+import { Switch, Route } from 'react-router-dom';
+
+import Layout from './components/Layout/Layout';
+import UserProfile from './components/Profile/UserProfile';
+import AuthPage from './pages/AuthPage';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
-    <div className="App">
-      <h1>This is Authentication in React </h1>
-    </div>
+    <Layout>
+      <Switch>
+        <Route path='/' exact>
+          <HomePage />
+        </Route>
+        <Route path='/auth'>
+          <AuthPage />
+        </Route>
+        <Route path='/profile'>
+          <UserProfile />
+        </Route>
+      </Switch>
+    </Layout>
   );
 }
 
