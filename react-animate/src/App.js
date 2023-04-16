f0eee5a161321668b3baaf3b2dc29da85650a153
Ankit Sharma <sharmaa7861@gmail.com>
2023-04-16 22:21:06 +0530

    changes committed

--- react-animate/src/App.js
+++ react-animate/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Transition } from "react-transition-group";
 
 import "./App.css";
 import Modal from "./components/Modal/Modal";
@@ -6,14 +7,54 @@ import Backdrop from "./components/Backdrop/Backdrop";
 import List from "./components/List/List";
 
 class App extends Component {
+  state = {
+    modalIsOpen: false,
+    showBlock: false,
+  };
+
+  showModal = () => {
+    this.setState({ modalIsOpen: true });
+  };
+
+  closeModal = () => {
+    this.setState({ modalIsOpen: false });
+  };
+
   render() {
     return (
       <div className="App">
         <h1>React Animations</h1>
-        <Modal />
-        <Backdrop />
-        <button className="Button">Open Modal</button>
+        <button
+          className="Button"
+          onClick={() =>
+            this.setState((prevState) => ({
+              showBlock: !prevState.showBlock,
+            }))
+          }
+        >
+          Toggle
+        </button>
+        <br />
+        <br />
+        
+          <Transition in={this.state.showBlock} timeout={300}>
+            {(state) => <p>{state}</p>}
+          </Transition>
+
+        {this.state.modalIsOpen ? (
+          <Modal show={this.state.modalIsOpen} closed={this.closeModal} />
+        ) : null}
+
+        {this.state.modalIsOpen ? (
+          <Backdrop show={this.state.modalIsOpen} />
+        ) : null}
+
+        <button className="Button" onClick={this.showModal}>
+          Open Modal
+        </button>
+
         <h3>Animating Lists</h3>
+
         <List />
       </div>
     );
